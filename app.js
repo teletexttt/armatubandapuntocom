d2eb87650a9bc7fb8bdf72f6db90b223ef3fab38
teletexttt <108497199+teletexttt@users.noreply.github.com>
2024-09-12 09:00:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-document.getElementById('title').addEventListener('click', function() {
-    var loginForm = document.getElementById('loginForm');
-    loginForm.style.display = 'block'; // Mostrar el formulario de login
-});
-
-// Opcional: Cerrar el formulario cuando se haga clic fuera de él
-document.addEventListener('click', function(event) {
-    var loginForm = document.getElementById('loginForm');
-    var title = document.getElementById('title');
-
-    if (!loginForm.contains(event.target) && event.target !== title) {
-        loginForm.style.display = 'none';
-    }
-});
+function showLogin() {
+    const loginForm = document.getElementById('login-form');
+    loginForm.classList.toggle('hidden');
+}
